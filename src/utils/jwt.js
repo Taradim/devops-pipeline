ec1eb7b8afbd28145c165d5cb96d0d50bf909904
Taradim <valerian.belles@gmail.com>
2025-11-27 22:08:47 +0100

    Secure the API with Arcjet

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -5,20 +5,20 @@ const JWT_SECRET = process.env.JWT_SECRET || 'devops-secret';
 const JWT_EXPIRATION = '1d';
 
 export const jwtToken = {
-    sign: (payload) => {
-        try {
-            return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRATION });
-        } catch (e) {
-            logger.error('failed to authenticate token', error);
-            throw new Error('failed to authenticate token');
-        }
-    },
-    verify: (token) => {
-        try {
-            return jwt.verify(token, JWT_SECRET);
-        } catch (e) {
-            logger.error('failed to verify token', error);
-            throw new Error('failed to verify token');
-        }
+  sign: payload => {
+    try {
+      return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRATION });
+    } catch (e) {
+      logger.error('failed to authenticate token', error);
+      throw new Error('failed to authenticate token');
     }
-}
+  },
+  verify: token => {
+    try {
+      return jwt.verify(token, JWT_SECRET);
+    } catch (e) {
+      logger.error('failed to verify token', error);
+      throw new Error('failed to verify token');
+    }
+  },
+};
